d061fdc8c41d181bd18dccf059e978802614e738
thatomatlala28 <32202321-thatomatlala28@users.noreply.replit.com>
2024-02-19 14:28:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+#name = "Lee"
+#print(name)
+#print("Hello, my name is " + name)
+
+#name1 = input(" what is my name: ")
+#print(name1)
+
+# Task 1 --. faherits to celcius
+
+  #temp_fahre =float (input ("Enter temperature in faheinheit: ")) 
+  
+  #temp_celc = float (temp_fahre - 32) * 5/9
+  
+  #print( str(temp_celc) + "°C") 
+
+#Simplyfy task 1
+  #print(f"The temperature in celcius is {temp_celc}°C")
+
+#Task 2 find the age of the person
+#year = input("Enter the year you were born: ")
+#age = 2024 - int(year)
+#print(f"You are {age} years old")
+#print(f"You are {2024 - int(year)} years old")
+
+#Task 3 -- area of the circle
+#import math
+#radius = input("Enter the radius of the circle: ")
+#print(f"The area of the circle is {math.pi * float(radius) ** 2}")
+
+# Task 3
+#value = int (input("Enter number: "))
+#n = value/ 10
+#empty = 10 - n
+#print("=" * int(n) + " " * int(empty) + " " + str(value) + "%")
+
+##Yolanda  solution
+#num = input("Enter a number ")
+#num1 = int(num) // 10
+#spaces = 10 - num1
+#eq = "=" * num1
+#sp = " " * spaces
+#print(f"{spaces} spaces left")
+#print("["+str(eq)+ sp +"] "  + num + "%")
+
+#####SLICING###
+quote= "I love python"
+print(quote[2:6])  #love
+print(quote[7:]) #python ( till the end)
+print(quote[-1: 4])
+
+###Skipping values##
+print(quote[0:6:2]) #skipping 2 values until 6 characters
+print(quote[::3]) #skipping 3 values until the end
+
+##Reversing##
+print(quote[::-1]) #reverse the string
