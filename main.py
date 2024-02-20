0c795ad6ca1498f9d93f3b3818c45fc4bc524e89
Thatom28 <107023437+Thatom28@users.noreply.github.com>
2024-02-20 07:16:12 +0000

    Slicing removed to another file

--- main.py
+++ main.py
@@ -40,17 +40,6 @@ spaces = 10 - num1
 eq = "=" * num1
 sp = " " * spaces
 print(f"{spaces} spaces left")
-print("["+str(eq)+ sp +"] "  + num + "%")
+#print("["+str(eq)+ sp +"] "  + num + "%")
 
-#####SLICING###
-quote= "I love python"
-print(quote[2:6])  #love
-print(quote[7:]) #python ( till the end)
-print(quote[-1: 4])
 
-###Skipping values##
-print(quote[0:6:2]) #skipping 2 values until 6 characters
-print(quote[::3]) #skipping 3 values until the end
-
-##Reversing##
-print(quote[::-1]) #reverse the string
