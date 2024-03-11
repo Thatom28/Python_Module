2da69bbe1d99dbc955f5b666c1628f6041933b33
Thatom28 <thatomatlala28@gmail.com>
2024-03-11 15:07:49 +0200

    numpy

--- day_15_blog_post.py
+++ day_15_blog_post.py
@@ -2,10 +2,18 @@ import json
 
 posts = {}
 with open("Day_15_blog_post.json", "r") as json_file:
-    posts = json.load(json_file)
+    blog = json.load(json_file)
 
-posts2 = {}
+posts = blog["posts"]
+summarizedPosts = []
 for post in posts:
-    posts2 = {**posts, "comments": (post["comments"]).count()}
+    summarizedPost = {
+        "title": post["title"],
+        "author": post["author"],
+        "number_of_commnets": len(post["comments"]),
+    }
+    summarizedPosts.append(summarizedPost)
 
-print(posts2)
+
+with open("summarized_posts.json", "w") as file:
+    json.dumps(summarizedPosts, file, indent=4)
